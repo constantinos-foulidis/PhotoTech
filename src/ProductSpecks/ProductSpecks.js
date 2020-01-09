1e4ea686479504bd19d21b46b16670180d583118
constantinos <constantine_flds@hotmail.com>
2020-01-09 20:34:49 +0200

    specks is ready

--- src/ProductSpecks/ProductSpecks.js
+++ src/ProductSpecks/ProductSpecks.js
@@ -1,9 +1,48 @@
-import React from 'react';
-import Dropdown from 'react-bootstrap/Dropdown'
-import '../Dropdawn/Dropdown.css';
-const ProductSpecs = (props) => {
-    return   (
-      <p>Hello from specks</p>
- )
-};
+import React, { Component } from 'react';
+import Button from 'react-bootstrap/Button';
+import '../ProductSpecks/ProductSpecks.css';
+
+class ProductSpecs extends Component {
+  constructor(){
+    super();
+
+  }
+  render () {
+    return (
+      <div className="Container">
+         <div className="row ">
+          <div className="col border">
+           <img   src="/PhotoSc.png"/>
+          </div>
+          <div className="col mb-2 border ml-4 ">
+               <div className="row">
+                 <div className="col border-bottom">
+                   <h4>Περιγραή Προιόντος</h4>
+                   <p >Φωτιστικό Τοιχου</p>
+                 </div>
+               </div>
+               <div className="row">
+                 <div className="col border-bottom mb-2">
+                   <h5>Κωδικος :</h5>
+                   <h5>Κaτηγορία :</h5>
+                   <h5>Υποκατηγορία :</h5>
+                   <h5>Διαθεσιμότητα :</h5>
+                   <h5>θέση :</h5>
+                   <h5>Τάξεις :</h5>
+                 </div>
+               </div>
+               <div className="row">
+                 <div className="col">
+                   <Button className="buttonWidth mb-2 mr-1" variant="info">Διαγραφή Προιόντος</Button>
+                   <Button className="buttonWidth mb-2" variant="info">Επεξεργασία αποθέματος</Button>
+                 </div>
+               </div>
+          </div>
+          </div>
+      </div>
+
+    );
+  }
+}
+
 export default ProductSpecs;
