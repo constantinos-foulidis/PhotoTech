8fb6ae48ffd337e8512356baf1b0c5a993d35b25
constantinos <constantine_flds@hotmail.com>
2020-01-11 17:14:59 +0200

    create modal for diathesimotita and add or remove suplies is ready

--- src/ProductSpecks/ProductSpecks.js
+++ src/ProductSpecks/ProductSpecks.js
@@ -1,12 +1,52 @@
 import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
 import '../ProductSpecks/ProductSpecks.css';
+import Modal from 'react-bootstrap/Modal'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
 
 class ProductSpecs extends Component {
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
+    this.handleShow = this.handleShow.bind(this);
+    		this.handleClose = this.handleClose.bind(this);
 
+    		this.state = {
+          remain:1000,
+          number:10,
+    			show: false,
+    		};
   }
+  handleClose() {
+  		this.setState({ show: false });
+  	}
+
+  	handleShow() {
+  		this.setState({ show: true });
+  	}
+
+addNumber=()=>{
+  console.log(this.state.number);
+   const firstRemain =Number(this.state.remain);
+   const addedValue = Number(this.state.number);
+   const sum=firstRemain+addedValue;
+   console.log(sum);
+   this.setState({remain:sum});
+}
+
+removeNumber=()=>{
+  const firstRemain =Number(this.state.remain);
+  const addedValue = Number(this.state.number);
+  const sum=firstRemain-addedValue;
+  this.setState({remain:sum});
+}
+
+numberChanged=(event)=>{
+ this.setState({number:event.target.value});
+ console.log("inside number changed");
+}
+
   render () {
     return (
       <div className="Container">
@@ -25,19 +65,19 @@ class ProductSpecs extends Component {
                  <div className="col border-bottom mb-2">
                    <div className="row">
                       <h5>Κωδικος :</h5>
-                      <p contentEditable={true} >12345</p>
+                      <p contentEditable={true} suppressContentEditableWarning={true} >12345</p>
                    </div>
                    <div className="row">
                    <h5>Κaτηγορία :</h5>
-                     <p contentEditable={true} >Τεστ</p>
+                     <p contentEditable={true}  suppressContentEditableWarning={true}>Τεστ</p>
                    </div>
                    <div className="row">
                    <h5>Υποκατηγορία :</h5>
-                     <p contentEditable={true} >Κυρια</p>
+                     <p contentEditable={true} suppressContentEditableWarning={true}>Κυρια</p>
                    </div>
                    <div className="row">
-                 <h5>Διαθεσιμότητα :</h5>
-                   <p>1000</p>
+                 <h5 onClick={this.handleShow}>Διαθεσιμότητα :</h5>
+                   <p>{this.state.remain}</p>
                  </div>
                  <div className="row">
                    <h5>θέση :</h5>
@@ -58,6 +98,34 @@ class ProductSpecs extends Component {
                </div>
           </div>
           </div>
+        <Modal  aria-labelledby="contained-modal-title-vcenter" show={this.state.show} onHide={this.handleClose}>
+    <Modal.Header closeButton>
+      <Modal.Title id="contained-modal-title-vcenter">
+      Επεξεργασια Αποθέματος
+         <h5>Συνολικό Απόθεμα {this.state.remain}</h5>
+      </Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+      <Container>
+        <Row className="show-grid mb-4">
+          <Col xs={12} md={8}>
+            <input type="number" placeholder="Αριθμος για αφαιρεση η προσθεση αποθεματος" value={this.state.number} onChange={(event) => this.numberChanged(event)}/>
+          </Col>
+        </Row>
+        <Row className="show-grid">
+          <Col xs={6} md={4}>
+           <Button className="mb-2 mr-1" variant="info" onClick={this.addNumber}>Πρόσθεση</Button>
+          </Col>
+          <Col xs={6} md={4}>
+           <Button className=" mb-2 mr-1" variant="info" onClick={this.removeNumber}>Αφαίρεση</Button>
+          </Col>
+        </Row>
+      </Container>
+    </Modal.Body>
+    <Modal.Footer>
+      <Button onClick={this.handleClose}>Close</Button>
+    </Modal.Footer>
+  </Modal>
       </div>
 
     );
