80818b9d3e6e615c9e3bcf8694ca4861582003f8
sofpanos <sofpanos@hotmail.com>
2020-01-15 15:21:21 +0200

    refactored to functional component
    Cause: Presentational component

--- src/components/ProductItem/ProductItem.js
+++ src/components/ProductItem/ProductItem.js
@@ -1,45 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './ProductItem.css';
 import Button from 'react-bootstrap/Button';
 import ExporPDF from '../ExportTOPdf/ExportToPdf'
 import ExportExcel from '../ExportTOExcel/ExportToExcel'
 
-class ProductItem extends Component {
-  constructor(props) {
-    super(props);
-    //  this.name = props.name;
-    //  this.refe  = props.refe;
-    console.log(props.refe);
-  }
-
-  handleSpecified = () => {
-    // alert('A name was submitted: ' + this.state.username+this.state.password);
-    //dad event.preventDefault();
-    //console.log(event.preventDefault());
-    this.props.history.push({
+const productItem = (props) => {
+  const handleSpecified = () =>
+    props.history.push({
       pathname: "/products/id",
-      //state: {username: this.state.username}
     });
-  }
 
-  render() {
-    return (
-      <div>
-        <ExporPDF />
-        <ExportExcel />
-        <div className="card cardWidth ml-5">
-          <img alt="logo" className="card-img-top imgWidth" src="/PhotoSc.png" />
-          <div className="card-body">
-            <h5 className="card-title">Φωτηστικο Τεστ</h5>
-            <p className="card-text">Διαθεσημοτητα : 150</p>
-            <p className="card-text border-bottom">Κωδικός : 123456</p>
-            <Button className="buttonWidth mb-2" variant="info">Διαγραφή Προιόντος</Button>
-            <Button className="buttonWidth mb-2" variant="info" onClick={this.handleSpecified}>Επεξεργασία αποθέματος</Button>
-          </div>
+  return (
+    <div>
+      <ExporPDF />
+      <ExportExcel />
+      <div className="card cardWidth ml-5">
+        <img alt="logo" className="card-img-top imgWidth" src="/PhotoSc.png" />
+        <div className="card-body">
+          <h5 className="card-title">Φωτηστικο Τεστ</h5>
+          <p className="card-text">Διαθεσημοτητα : 150</p>
+          <p className="card-text border-bottom">Κωδικός : 123456</p>
+          <Button className="buttonWidth mb-2" variant="info">Διαγραφή Προιόντος</Button>
+          <Button className="buttonWidth mb-2" variant="info" onClick={ handleSpecified }>Επεξεργασία αποθέματος</Button>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-export default ProductItem;
+
+export default productItem;
