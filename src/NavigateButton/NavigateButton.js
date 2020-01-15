4d8b616bf821ee6b75395e5f0bae493d67f42b0c
sofpanos <sofpanos@hotmail.com>
2020-01-15 14:27:07 +0200

    some minor changes

--- src/NavigateButton/NavigateButton.js
+++ src/NavigateButton/NavigateButton.js
@@ -6,16 +6,12 @@ import { withRouter } from "react-router-dom";
 class NavigateButton extends Component {
 
   handleNewItem = () => {
-    console.log(this.props);
     this.props.history.push("/products/add");
   }
 
   render() {
     return (
-
       <Button className="widthbtn mb-2" variant="secondary" onClick={this.handleNewItem}>Προσθήκη νέου</Button>
-
-
     );
   }
 }
--- src/ProductsRecources/ProductRecources.js
+++ src/ProductsRecources/ProductRecources.js
@@ -10,24 +10,16 @@ import AddNewUser from '../AddNewUser/AddNewUser';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
-import NavigateButton from '../NavigateButton/NavigateButton';
 import { Route } from "react-router-dom";
 import UserHandle from '../UserHandler/UserHandler'
 import Category from '../Data/Category'
-class Products extends Component {
-  constructor(props) {
-    super(props);
-    //  if(!this.props.location.state.username){
-    //  const state = this.props.location.state.username;
-    //  console.log(state);
-    //}
 
-  }
+class Products extends Component {
+
   handleAddnew = () => {
     console.log(this.props);
     this.props.history.push({
       pathname: "/products/add",
-      //state: {username: this.state.username}
     });
 
   }
@@ -46,10 +38,8 @@ class Products extends Component {
         </Row>
         <Row className="offset-1">
           {Category.map((category, index) => (<Dropdawn key={category.name} category={category} />))}
-
           <Button className="widthbtn mb-2" variant="secondary" onClick={this.handleAddnew}>Προσθήκη νέου</Button>
         </Row>
-
         <Row className="offset-1">
           <Route exact path="/products" component={ProductItem} />
           <Route path="/products/add" component={NewItem} />
@@ -57,8 +47,8 @@ class Products extends Component {
           <Route path="/products/adduser" component={AddNewUser} />
           <Route path="/products/handleuser" component={UserHandle} />
         </Row>
-
-      </Container>);
+      </Container>
+    );
   }
 }
 
