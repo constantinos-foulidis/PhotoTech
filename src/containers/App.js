ca506faea2a8129b4ffa4e7c68c1ea4f16888c5b
Sofianos Panagiotis <psofianos@ADDC.NETWORK.ARXNET>
2020-01-21 13:22:16 +0200

    minor change

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import './App.css';
-import MainMenu from './MainMenu/MainMenu';
-import ResourceProgramMenu from './ResourceProgram/ResourceProgramMenu/ResourceProgramMenu';
-import Login from '../components/Login/Login'
+import React from "react";
+import "./App.css";
+import MainMenu from "./MainMenu/MainMenu";
+import ResourceProgramMenu from "./ResourceProgram/ResourceProgramMenu/ResourceProgramMenu";
+import Login from "../components/Login/Login";
 
-import {
-  BrowserRouter as Router,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 function App() {
   return (
-
     <div className="App">
       <header className="App-header shadow p-3 mb-5 ">
         <img alt="logo" className="headerpic" src="/PhotoSc.png" />
       </header>
       <Router>
-        <Route exact path="/" component={MainMenu} />
-        <Route path="/recource" component={ResourceProgramMenu} />
-        <Route path="/simple_user" component={Login} />
-        <Route render={()=><p>Page Not Found</p>} />
+        <Switch>
+          <Route exact path="/" component={MainMenu} />
+          <Route path="/recource" component={ResourceProgramMenu} />
+          <Route path="/simple_user" component={Login} />
+          <Route component={(props) => <p>Page Not Found</p>} />
+        </Switch>
       </Router>
     </div>
   );
--- src/containers/ResourceProgram/ResourceProgramMenu/ResourceProgramMenu.js
+++ src/containers/ResourceProgram/ResourceProgramMenu/ResourceProgramMenu.js
@@ -1,27 +1,34 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import Cards from '../../../components/Card/Card';
-import UserManagement from '../User Management/UserHandler/UserHandler';
-import Products from '../Product Management/ProductsRecources/ProductRecources';
-import Login from '../../../components/Login/Login';
-const ResourceProgramMenu = (props) => {
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Cards from "../../../components/Card/Card";
+import UserManagement from "../User Management/UserHandler/UserHandler";
+import Products from "../Product Management/ProductsRecources/ProductRecources";
+import Login from "../../../components/Login/Login";
+const ResourceProgramMenu = props => {
   console.log(props);
 
   return (
     <React.Fragment>
-      <Route path={props.match.url} exact render={() => (
-        <div className="Container">
-          <h2 className="mb-5 text-center">Επιλέξτε Κατηγορία</h2>
-          <div className="row justify-content-center">
-            <Cards refe={props.match.url + "/admin_user"} name="Διαχηρηστής Αποθήκης" />
-            <Cards refe={props.match.url + "/simple_user"} name="Χρήστης Αποθήκης" />
-          </div>
-        </div>
-      )} />
-      <Route path={props.match.url + "/simple_user"} component={Login} />
-      <Route path={props.match.url + "/user_management"}  component={UserManagement} />
-      <Route path={props.match.url + "/products"}  component={Products} />
+      <Switch>
+        <Route
+          path={props.match.url}
+          exact
+          render={() => (
+            <div className="Container">
+              <h2 className="mb-5 text-center">Επιλέξτε Κατηγορία</h2>
+              <div className="row justify-content-center">
+                <Cards refe={props.match.url + "/admin_user"} name="Διαχηρηστής Αποθήκης" />
+                <Cards refe={props.match.url + "/simple_user"} name="Χρήστης Αποθήκης" />
+              </div>
+            </div>
+          )}
+        />
+        <Route path={props.match.url + "/simple_user"} component={Login} />
+        <Route path={props.match.url + "/user_management"} component={UserManagement} />
+        <Route path={props.match.url + "/products"} component={Products} />
+        <Route component={(props) => <p>Page Not Found</p>} />
+      </Switch>
     </React.Fragment>
-  )
+  );
 };
 export default ResourceProgramMenu;
