1b260840b8a80f5c54439119f88b290ba841ac1f
Softexpert-code <leo@softexpert.gr>
2020-02-27 13:06:21 +0200

    Typos

--- src/containers/MainMenu/MainMenu.js
+++ src/containers/MainMenu/MainMenu.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react';
-import './MainMenu.css';
-import Cards from '../../components/Card/Card';
-
+import React, { Component } from "react";
+import "./MainMenu.css";
+import Cards from "../../components/Card/Card";
 
 class MainMenu extends Component {
   render() {
     return (
-
       <div className="Container ">
         <h2 className="mb-5 text-center">Επιλέξτε Κατηγορία</h2>
         <div className="row justify-content-center">
           <div className="col">
-            <Cards refe="/admin" name="Διαχηρηστής Προγράμματος" />
+            <Cards refe="/admin" name="Διαχειριστής Προγράμματος" />
           </div>
           <div className="col">
             <Cards refe="/lab" name="Εργαστήριο Αιγινίου" />
           </div>
           <div className="col">
-            <Cards refe="/office" name="Γραφειο Θεσσαλονικης" />
+            <Cards refe="/office" name="Γραφείο Θεσσαλονικης" />
           </div>
         </div>
         <div className="row justify-content-center">
@@ -32,7 +30,6 @@ class MainMenu extends Component {
           </div>
         </div>
       </div>
-
     );
   }
 }
