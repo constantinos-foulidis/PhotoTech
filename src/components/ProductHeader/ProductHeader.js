16b51bdead91e6163af91d352aa706ed71a8af99
Softexpert-code <leo@softexpert.gr>
2020-03-04 10:58:35 +0200

    Fonts & typos

--- src/components/ProductHeader/ProductHeader.js
+++ src/components/ProductHeader/ProductHeader.js
@@ -1,42 +1,48 @@
-import React,{Component} from 'react';
-import Row from 'react-bootstrap/Row';
-import Category from '../../Data/Category'
-import Dropdawn from '../Dropdawn/Dropdawn';
-import Button from 'react-bootstrap/Button';
+import React, { Component } from "react";
+import Row from "react-bootstrap/Row";
+import Category from "../../Data/Category";
+import Dropdawn from "../Dropdawn/Dropdawn";
+import Button from "react-bootstrap/Button";
 import { withRouter } from "react-router";
-import Container from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Col';
-import { navigationToggleButton as NavigationToggleButton } from '../NavigationDrawer/NavigationDrawer';
-class ProductHeader  extends Component {
-
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import { navigationToggleButton as NavigationToggleButton } from "../NavigationDrawer/NavigationDrawer";
+class ProductHeader extends Component {
   handleAddnew = () => {
     console.log(this.props);
     this.props.history.push({
-      pathname: "/recource/products/add",
+      pathname: "/recource/products/add"
       //state: {username: this.state.username}
     });
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <Container className="mb-5">
+          <div className="headerCenter mb-4">
+            <h1>Προϊόντα:</h1>
+          </div>
 
-  }
-  render(){
-    return   (
-  <React.Fragment>
-   <Container className="mb-5">
-   <div className="headerCenter mb-4">
-    <h1>Προιόντα:</h1>
-    </div>
-
-        <Row sm={4}>
-  <NavigationToggleButton/>
-       </Row>
-        <Col className="justify-content-start offset-3">
-       <Row md={10} >
-        {Category.map((category, index) => (<Dropdawn  key={category.name} category={category}/>))}
-        <Button className="widthbtn mb-2" variant="secondary" onClick={this.handleAddnew}>Προσθήκη νέου</Button>
-        </Row>
-      </Col>
-      </Container>
+          <Row sm={4}>
+            <NavigationToggleButton />
+          </Row>
+          <Col className="justify-content-start offset-3">
+            <Row md={10}>
+              {Category.map((category, index) => (
+                <Dropdawn key={category.name} category={category} />
+              ))}
+              <Button
+                className="widthbtn mb-2"
+                variant="secondary"
+                onClick={this.handleAddnew}
+              >
+                Προσθήκη νέου
+              </Button>
+            </Row>
+          </Col>
+        </Container>
       </React.Fragment>
- )
+    );
+  }
 }
-};
 export default withRouter(ProductHeader);
