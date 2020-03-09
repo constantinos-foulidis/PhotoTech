670754c81c4337eae43c813ace6b076b4f64cf15
Softexpert-code <leo@softexpert.gr>
2020-03-09 14:31:24 +0200

    Changes in postitions

--- src/Data/PositionForUpdate.js
+++ src/Data/PositionForUpdate.js
@@ -1,4 +1,5 @@
-const positions = [{
+const positions = [
+  {
     label: "A11",
     value: "A11"
   },
@@ -9,30 +10,6 @@ const positions = [{
   {
     label: "A13",
     value: "A13"
-  },
-  {
-    label: "A14",
-    value: "A14"
-  },
-  {
-    label: "A15",
-    value: "A15"
-  },
-  {
-    label: "A16",
-    value: "A16"
-  },
-  {
-    label: "A17",
-    value: "A17"
-  },
-  {
-    label: "A18",
-    value: "A18"
-  },
-  {
-    label: "A19",
-    value: "A19"
   },
   {
     label: "A21",
@@ -47,30 +24,6 @@ const positions = [{
     value: "A23"
   },
   {
-    label: "A24",
-    value: "A24"
-  },
-  {
-    label: "A25",
-    value: "A25"
-  },
-  {
-    label: "A26",
-    value: "A26"
-  },
-  {
-    label: "A27",
-    value: "A27"
-  },
-  {
-    label: "A28",
-    value: "A28"
-  },
-  {
-    label: "A29",
-    value: "A29"
-  },
-  {
     label: "A31",
     value: "A31"
   },
@@ -81,30 +34,6 @@ const positions = [{
   {
     label: "A33",
     value: "A33"
-  },
-  {
-    label: "A34",
-    value: "A34"
-  },
-  {
-    label: "A35",
-    value: "A35"
-  },
-  {
-    label: "A36",
-    value: "A36"
-  },
-  {
-    label: "A37",
-    value: "A37"
-  },
-  {
-    label: "A38",
-    value: "A38"
-  },
-  {
-    label: "A39",
-    value: "A39"
   },
   {
     label: "A41",
@@ -119,30 +48,6 @@ const positions = [{
     value: "A43"
   },
   {
-    label: "A44",
-    value: "A44"
-  },
-  {
-    label: "A45",
-    value: "A45"
-  },
-  {
-    label: "A46",
-    value: "A46"
-  },
-  {
-    label: "A47",
-    value: "A347"
-  },
-  {
-    label: "A48",
-    value: "A48"
-  },
-  {
-    label: "A49",
-    value: "A49"
-  },
-  {
     label: "A51",
     value: "A51"
   },
@@ -153,30 +58,6 @@ const positions = [{
   {
     label: "A53",
     value: "A53"
-  },
-  {
-    label: "A54",
-    value: "A54"
-  },
-  {
-    label: "A55",
-    value: "A55"
-  },
-  {
-    label: "A56",
-    value: "A56"
-  },
-  {
-    label: "A57",
-    value: "A57"
-  },
-  {
-    label: "A58",
-    value: "A58"
-  },
-  {
-    label: "A59",
-    value: "A59"
   },
   {
     label: "A61",
@@ -191,30 +72,6 @@ const positions = [{
     value: "A63"
   },
   {
-    label: "A64",
-    value: "A64"
-  },
-  {
-    label: "A65",
-    value: "A65"
-  },
-  {
-    label: "A66",
-    value: "A66"
-  },
-  {
-    label: "A67",
-    value: "A67"
-  },
-  {
-    label: "A68",
-    value: "A68"
-  },
-  {
-    label: "A69",
-    value: "A69"
-  },
-  {
     label: "A71",
     value: "A71"
   },
@@ -225,30 +82,6 @@ const positions = [{
   {
     label: "A73",
     value: "A73"
-  },
-  {
-    label: "A74",
-    value: "A74"
-  },
-  {
-    label: "A75",
-    value: "A75"
-  },
-  {
-    label: "A76",
-    value: "A76"
-  },
-  {
-    label: "A77",
-    value: "A77"
-  },
-  {
-    label: "A78",
-    value: "A78"
-  },
-  {
-    label: "A79",
-    value: "A79"
   },
   {
     label: "A81",
@@ -263,30 +96,6 @@ const positions = [{
     value: "A83"
   },
   {
-    label: "A84",
-    value: "A84"
-  },
-  {
-    label: "A85",
-    value: "A85"
-  },
-  {
-    label: "A86",
-    value: "A86"
-  },
-  {
-    label: "A87",
-    value: "A87"
-  },
-  {
-    label: "A88",
-    value: "A88"
-  },
-  {
-    label: "A89",
-    value: "A89"
-  },
-  {
     label: "A91",
     value: "A91"
   },
@@ -299,250 +108,100 @@ const positions = [{
     value: "A93"
   },
   {
-    label: "A94",
-    value: "A94"
-  },
-  {
-    label: "A95",
-    value: "A95"
-  },
-  {
-    label: "A96",
-    value: "A96"
-  },
-  {
-    label: "A97",
-    value: "A97"
-  },{
-    label: "A98",
-    value: "A98"
-  },{
-    label: "A99",
-    value: "A99"
-  },
-  {
     label: "A101",
     value: "A101"
   },
   {
-    label: "Β11",
-    value: "Β11"
+    label: "A102",
+    value: "A102"
   },
   {
-    label: "Β12",
-    value: "Β12"
+    label: "A103",
+    value: "A103"
   },
   {
-    label: "Β13",
-    value: "Β13"
+    label: "B11",
+    value: "B11"
   },
   {
-    label: "Β14",
-    value: "Β14"
+    label: "B12",
+    value: "B12"
   },
   {
-    label: "Β15",
-    value: "Β15"
+    label: "B13",
+    value: "B13"
   },
   {
-    label: "Β16",
-    value: "Β16"
+    label: "B21",
+    value: "B21"
   },
   {
-    label: "Β17",
-    value: "Β17"
+    label: "B22",
+    value: "B22"
   },
   {
-    label: "Β18",
-    value: "Β18"
+    label: "B23",
+    value: "B23"
   },
   {
-    label: "Β19",
-    value: "Β19"
+    label: "B31",
+    value: "B31"
   },
   {
-    label: "Β21",
-    value: "Β21"
+    label: "B32",
+    value: "B32"
   },
   {
-    label: "Β22",
-    value: "Β22"
+    label: "B33",
+    value: "B33"
   },
   {
-    label: "Β23",
-    value: "Β23"
+    label: "B41",
+    value: "B41"
   },
   {
-    label: "Β24",
-    value: "Β24"
+    label: "B42",
+    value: "B42"
   },
   {
-    label: "Β25",
-    value: "Β25"
+    label: "B43",
+    value: "B43"
   },
   {
-    label: "Β26",
-    value: "Β26"
+    label: "B51",
+    value: "B51"
   },
   {
-    label: "Β27",
-    value: "Β27"
+    label: "B52",
+    value: "B52"
   },
   {
-    label: "Β28",
-    value: "Β28"
+    label: "B53",
+    value: "B53"
   },
   {
-    label: "Β29",
-    value: "Β29"
+    label: "B61",
+    value: "B61"
   },
   {
-    label: "Β31",
-    value: "Β31"
+    label: "B62",
+    value: "B62"
   },
   {
-    label: "Β32",
-    value: "Β32"
+    label: "B63",
+    value: "B63"
   },
   {
-    label: "Β33",
-    value: "Β33"
+    label: "B71",
+    value: "B71"
   },
   {
-    label: "Β34",
-    value: "Β34"
+    label: "B72",
+    value: "B72"
   },
   {
-    label: "Β35",
-    value: "Β35"
-  },
-  {
-    label: "Β36",
-    value: "Β36"
-  },
-  {
-    label: "Β37",
-    value: "Β37"
-  },
-  {
-    label: "Β38",
-    value: "Β38"
-  },
-  {
-    label: "Β39",
-    value: "Β39"
-  },
-  {
-    label: "Β41",
-    value: "Β41"
-  },
-  {
-    label: "Β42",
-    value: "Β42"
-  },
-  {
-    label: "Β43",
-    value: "Β43"
-  },
-  {
-    label: "Β44",
-    value: "Β44"
-  },
-  {
-    label: "Β45",
-    value: "Β45"
-  },
-  {
-    label: "Β46",
-    value: "Β46"
-  },
-  {
-    label: "Β47",
-    value: "Β47"
-  },
-  {
-    label: "Β48",
-    value: "Β48"
-  },
-  {
-    label: "Β49",
-    value: "Β49"
-  },
-  {
-    label: "Β51",
-    value: "Β51"
-  },
-  {
-    label: "Β52",
-    value: "Β52"
-  },
-  {
-    label: "Β53",
-    value: "Β53"
-  },
-  {
-    label: "Β54",
-    value: "Β54"
-  },
-  {
-    label: "Β55",
-    value: "Β55"
-  },
-  {
-    label: "Β56",
-    value: "Β56"
-  },
-  {
-    label: "Β57",
-    value: "Β57"
-  },
-  {
-    label: "Β58",
-    value: "Β58"
-  },
-  {
-    label: "Β59",
-    value: "Β59"
-  },
-  {
-    label: "Β61",
-    value: "Β61"
-  },
-  {
-    label: "Β62",
-    value: "Β62"
-  },
-  {
-    label: "Β63",
-    value: "Β63"
-  },
-  {
-    label: "Β64",
-    value: "Β64"
-  },
-  {
-    label: "Β65",
-    value: "Β65"
-  },
-  {
-    label: "Β66",
-    value: "Β66"
-  },
-  {
-    label: "Β67",
-    value: "Β67"
-  },
-  {
-    label: "Β68",
-    value: "Β68"
-  },
-  {
-    label: "Β69",
-    value: "Β69"
-  },
-  {
-    label: "Β71",
-    value: "Β71"
+    label: "B73",
+    value: "B73"
   },
   {
     label: "Γ11",
@@ -557,30 +216,6 @@ const positions = [{
     value: "Γ13"
   },
   {
-    label: "Γ14",
-    value: "Γ14"
-  },
-  {
-    label: "Γ15",
-    value: "Γ15"
-  },
-  {
-    label: "Γ16",
-    value: "Γ16"
-  },
-  {
-    label: "Γ17",
-    value: "Γ17"
-  },
-  {
-    label: "Γ18",
-    value: "Γ18"
-  },
-  {
-    label: "Γ19",
-    value: "Γ19"
-  },
-  {
     label: "Γ21",
     value: "Γ21"
   },
@@ -591,30 +226,6 @@ const positions = [{
   {
     label: "Γ23",
     value: "Γ23"
-  },
-  {
-    label: "Γ24",
-    value: "Γ24"
-  },
-  {
-    label: "Γ25",
-    value: "Γ25"
-  },
-  {
-    label: "Γ26",
-    value: "Γ26"
-  },
-  {
-    label: "Γ27",
-    value: "Γ27"
-  },
-  {
-    label: "Γ28",
-    value: "Γ28"
-  },
-  {
-    label: "Γ29",
-    value: "Γ29"
   },
   {
     label: "Γ31",
@@ -629,30 +240,6 @@ const positions = [{
     value: "Γ33"
   },
   {
-    label: "Γ34",
-    value: "Γ34"
-  },
-  {
-    label: "Γ35",
-    value: "Γ35"
-  },
-  {
-    label: "Γ36",
-    value: "Γ36"
-  },
-  {
-    label: "Γ37",
-    value: "Γ37"
-  },
-  {
-    label: "Γ38",
-    value: "Γ38"
-  },
-  {
-    label: "Γ39",
-    value: "Γ39"
-  },
-  {
     label: "Γ41",
     value: "Γ41"
   },
@@ -663,30 +250,6 @@ const positions = [{
   {
     label: "Γ43",
     value: "Γ43"
-  },
-  {
-    label: "Γ44",
-    value: "Γ44"
-  },
-  {
-    label: "Γ45",
-    value: "Γ45"
-  },
-  {
-    label: "Γ46",
-    value: "Γ46"
-  },
-  {
-    label: "Γ47",
-    value: "Γ47"
-  },
-  {
-    label: "Γ48",
-    value: "Γ48"
-  },
-  {
-    label: "Γ49",
-    value: "Γ49"
   },
   {
     label: "Γ51",
@@ -701,30 +264,6 @@ const positions = [{
     value: "Γ53"
   },
   {
-    label: "Γ54",
-    value: "Γ54"
-  },
-  {
-    label: "Γ55",
-    value: "Γ55"
-  },
-  {
-    label: "Γ56",
-    value: "Γ56"
-  },
-  {
-    label: "Γ57",
-    value: "Γ57"
-  },
-  {
-    label: "Γ58",
-    value: "Γ58"
-  },
-  {
-    label: "Γ59",
-    value: "Γ59"
-  },
-  {
     label: "Γ61",
     value: "Γ61"
   },
@@ -737,32 +276,16 @@ const positions = [{
     value: "Γ63"
   },
   {
-    label: "Γ64",
-    value: "Γ64"
-  },
-  {
-    label: "Γ65",
-    value: "Γ65"
-  },
-  {
-    label: "Γ66",
-    value: "Γ66"
-  },
-  {
-    label: "Γ67",
-    value: "Γ67"
-  },
-  {
-    label: "Γ68",
-    value: "Γ68"
-  },
-  {
-    label: "Γ69",
-    value: "Γ69"
-  },
-  {
     label: "Γ71",
     value: "Γ71"
+  },
+  {
+    label: "Γ72",
+    value: "Γ72"
+  },
+  {
+    label: "Γ73",
+    value: "Γ73"
   },
   {
     label: "Δ11",
@@ -777,30 +300,6 @@ const positions = [{
     value: "Δ13"
   },
   {
-    label: "Δ14",
-    value: "Δ14"
-  },
-  {
-    label: "Δ15",
-    value: "Δ15"
-  },
-  {
-    label: "Δ16",
-    value: "Δ16"
-  },
-  {
-    label: "Δ17",
-    value: "Δ17"
-  },
-  {
-    label: "Δ18",
-    value: "Δ18"
-  },
-  {
-    label: "Δ19",
-    value: "Δ19"
-  },
-  {
     label: "Δ21",
     value: "Δ21"
   },
@@ -811,30 +310,6 @@ const positions = [{
   {
     label: "Δ23",
     value: "Δ23"
-  },
-  {
-    label: "Δ24",
-    value: "Δ24"
-  },
-  {
-    label: "Δ25",
-    value: "Δ25"
-  },
-  {
-    label: "Δ26",
-    value: "Δ26"
-  },
-  {
-    label: "Δ27",
-    value: "Δ27"
-  },
-  {
-    label: "Δ28",
-    value: "Δ28"
-  },
-  {
-    label: "Δ29",
-    value: "Δ29"
   },
   {
     label: "Δ31",
@@ -849,30 +324,6 @@ const positions = [{
     value: "Δ33"
   },
   {
-    label: "Δ34",
-    value: "Δ34"
-  },
-  {
-    label: "Δ35",
-    value: "Δ35"
-  },
-  {
-    label: "Δ36",
-    value: "Δ36"
-  },
-  {
-    label: "Δ37",
-    value: "Δ37"
-  },
-  {
-    label: "Δ38",
-    value: "Δ38"
-  },
-  {
-    label: "Δ39",
-    value: "Δ39"
-  },
-  {
     label: "Δ41",
     value: "Δ41"
   },
@@ -883,30 +334,6 @@ const positions = [{
   {
     label: "Δ43",
     value: "Δ43"
-  },
-  {
-    label: "Δ44",
-    value: "Δ44"
-  },
-  {
-    label: "Δ45",
-    value: "Δ45"
-  },
-  {
-    label: "Δ46",
-    value: "Δ46"
-  },
-  {
-    label: "Δ47",
-    value: "Δ47"
-  },
-  {
-    label: "Δ48",
-    value: "Δ48"
-  },
-  {
-    label: "Δ49",
-    value: "Δ49"
   },
   {
     label: "Δ51",
@@ -921,30 +348,6 @@ const positions = [{
     value: "Δ53"
   },
   {
-    label: "Δ54",
-    value: "Δ54"
-  },
-  {
-    label: "Δ55",
-    value: "Δ55"
-  },
-  {
-    label: "Δ56",
-    value: "Δ56"
-  },
-  {
-    label: "Δ57",
-    value: "Δ57"
-  },
-  {
-    label: "Δ58",
-    value: "Δ58"
-  },
-  {
-    label: "Δ59",
-    value: "Δ59"
-  },
-  {
     label: "Δ61",
     value: "Δ61"
   },
@@ -955,30 +358,6 @@ const positions = [{
   {
     label: "Δ63",
     value: "Δ63"
-  },
-  {
-    label: "Δ64",
-    value: "Δ64"
-  },
-  {
-    label: "Δ65",
-    value: "Δ65"
-  },
-  {
-    label: "Δ66",
-    value: "Δ66"
-  },
-  {
-    label: "Δ67",
-    value: "Δ67"
-  },
-  {
-    label: "Δ68",
-    value: "Δ68"
-  },
-  {
-    label: "Δ69",
-    value: "Δ69"
   },
   {
     label: "Δ71",
@@ -992,34 +371,7 @@ const positions = [{
     label: "Δ73",
     value: "Δ73"
   },
-  {
-    label: "Δ74",
-    value: "Δ74"
-  },
-  {
-    label: "Δ75",
-    value: "Δ75"
-  },
-  {
-    label: "Δ76",
-    value: "Δ76"
-  },
-  {
-    label: "Δ77",
-    value: "Δ77"
-  },
-  {
-    label: "Δ78",
-    value: "Δ78"
-  },
-  {
-    label: "Δ79",
-    value: "Δ79"
-  },
-  {
-    label: "Δ81",
-    value: "Δ81"
-  },
+  { label: "Δ81", value: "Δ81" },
   {
     label: "Δ82",
     value: "Δ82"
@@ -1027,30 +379,6 @@ const positions = [{
   {
     label: "Δ83",
     value: "Δ83"
-  },
-  {
-    label: "Δ84",
-    value: "Δ84"
-  },
-  {
-    label: "Δ85",
-    value: "Δ85"
-  },
-  {
-    label: "Δ86",
-    value: "Δ86"
-  },
-  {
-    label: "Δ87",
-    value: "Δ87"
-  },
-  {
-    label: "Δ88",
-    value: "Δ88"
-  },
-  {
-    label: "Δ89",
-    value: "Δ89"
   },
   {
     label: "Δ91",
@@ -1065,30 +393,6 @@ const positions = [{
     value: "Δ93"
   },
   {
-    label: "Δ94",
-    value: "Δ94"
-  },
-  {
-    label: "Δ95",
-    value: "Δ95"
-  },
-  {
-    label: "Δ96",
-    value: "Δ96"
-  },
-  {
-    label: "Δ97",
-    value: "Δ97"
-  },
-  {
-    label: "Δ98",
-    value: "Δ98"
-  },
-  {
-    label: "Δ99",
-    value: "Δ99"
-  },
-  {
     label: "Δ101",
     value: "Δ101"
   },
@@ -1101,30 +405,6 @@ const positions = [{
     value: "Δ103"
   },
   {
-    label: "Δ104",
-    value: "Δ104"
-  },
-  {
-    label: "Δ105",
-    value: "Δ105"
-  },
-  {
-    label: "Δ106",
-    value: "Δ106"
-  },
-  {
-    label: "Δ107",
-    value: "Δ107"
-  },
-  {
-    label: "Δ108",
-    value: "Δ108"
-  },
-  {
-    label: "Δ109",
-    value: "Δ109"
-  },
-  {
     label: "Δ111",
     value: "Δ111"
   },
@@ -1134,35 +414,19 @@ const positions = [{
   },
   {
     label: "Δ113",
-    value: "Δ113"
-  },
-  {
-    label: "Δ114",
-    value: "Δ114"
-  },
-  {
-    label: "Δ115",
-    value: "Δ115"
-  },
-  {
-    label: "Δ116",
-    value: "Δ116"
-  },
-  {
-    label: "Δ117",
-    value: "Δ117"
-  },
-  {
-    label: "Δ118",
-    value: "Δ118"
-  },
-  {
-    label: "Δ119",
-    value: "Δ119"
+    value: "A43"
   },
   {
     label: "Δ121",
-    value: "Δ121"
+    value: "Δ122"
+  },
+  {
+    label: "Δ122",
+    value: "Δ122"
+  },
+  {
+    label: "Δ123",
+    value: "Δ123"
   },
   {
     label: "Ε11",
@@ -1177,30 +441,6 @@ const positions = [{
     value: "Ε13"
   },
   {
-    label: "Ε14",
-    value: "Ε14"
-  },
-  {
-    label: "Ε15",
-    value: "Ε15"
-  },
-  {
-    label: "Ε16",
-    value: "Ε16"
-  },
-  {
-    label: "Ε17",
-    value: "Ε17"
-  },
-  {
-    label: "Ε18",
-    value: "Ε18"
-  },
-  {
-    label: "Ε19",
-    value: "Ε19"
-  },
-  {
     label: "Ε21",
     value: "Ε21"
   },
@@ -1211,30 +451,6 @@ const positions = [{
   {
     label: "Ε23",
     value: "Ε23"
-  },
-  {
-    label: "Ε24",
-    value: "Ε24"
-  },
-  {
-    label: "Ε25",
-    value: "Ε25"
-  },
-  {
-    label: "Ε26",
-    value: "Ε26"
-  },
-  {
-    label: "Ε27",
-    value: "Ε27"
-  },
-  {
-    label: "Ε28",
-    value: "Ε28"
-  },
-  {
-    label: "Ε29",
-    value: "Ε29"
   },
   {
     label: "Ε31",
@@ -1249,30 +465,6 @@ const positions = [{
     value: "Ε33"
   },
   {
-    label: "Ε34",
-    value: "Ε34"
-  },
-  {
-    label: "Ε35",
-    value: "Ε35"
-  },
-  {
-    label: "Ε36",
-    value: "Ε36"
-  },
-  {
-    label: "Ε37",
-    value: "Ε37"
-  },
-  {
-    label: "Ε38",
-    value: "Ε38"
-  },
-  {
-    label: "Ε39",
-    value: "Ε39"
-  },
-  {
     label: "Ε41",
     value: "Ε41"
   },
@@ -1285,32 +477,16 @@ const positions = [{
     value: "Ε43"
   },
   {
-    label: "Ε44",
-    value: "Ε44"
-  },
-  {
-    label: "Ε45",
-    value: "Ε45"
-  },
-  {
-    label: "Ε46",
-    value: "Ε46"
-  },
-  {
-    label: "Ε47",
-    value: "Ε47"
-  },
-  {
-    label: "Ε48",
-    value: "Ε48"
-  },
-  {
-    label: "Ε49",
-    value: "Ε49"
-  },
-  {
     label: "Ε51",
     value: "Ε51"
+  },
+  {
+    label: "Ε52",
+    value: "Ε52"
+  },
+  {
+    label: "Ε53",
+    value: "Ε53"
   },
   {
     label: "Ζ11",
@@ -1325,32 +501,16 @@ const positions = [{
     value: "Ζ13"
   },
   {
-    label: "Ζ14",
-    value: "Ζ14"
-  },
-  {
-    label: "Ζ15",
-    value: "Ζ15"
-  },
-  {
-    label: "Ζ16",
-    value: "Ζ16"
-  },
-  {
-    label: "Ζ17",
-    value: "Ζ17"
-  },
-  {
-    label: "Ζ18",
-    value: "Ζ18"
-  },
-  {
-    label: "Ζ19",
-    value: "Ζ19"
-  },
-  {
     label: "Ζ21",
     value: "Ζ21"
+  },
+  {
+    label: "Ζ22",
+    value: "Ζ22"
+  },
+  {
+    label: "Ζ23",
+    value: "Ζ23"
   },
   {
     label: "Θ11",
@@ -1365,30 +525,6 @@ const positions = [{
     value: "Θ13"
   },
   {
-    label: "Θ14",
-    value: "Θ14"
-  },
-  {
-    label: "Θ15",
-    value: "Θ15"
-  },
-  {
-    label: "Θ16",
-    value: "Θ16"
-  },
-  {
-    label: "Θ17",
-    value: "Θ17"
-  },
-  {
-    label: "Θ18",
-    value: "Θ18"
-  },
-  {
-    label: "Θ19",
-    value: "Θ19"
-  },
-  {
     label: "Θ21",
     value: "Θ21"
   },
@@ -1399,30 +535,6 @@ const positions = [{
   {
     label: "Θ23",
     value: "Θ23"
-  },
-  {
-    label: "Θ24",
-    value: "Θ24"
-  },
-  {
-    label: "Θ25",
-    value: "Θ25"
-  },
-  {
-    label: "Θ26",
-    value: "Θ26"
-  },
-  {
-    label: "Θ27",
-    value: "Θ27"
-  },
-  {
-    label: "Θ28",
-    value: "Θ28"
-  },
-  {
-    label: "Θ29",
-    value: "Θ29"
   },
   {
     label: "Θ31",
@@ -1437,33 +549,16 @@ const positions = [{
     value: "Θ33"
   },
   {
-    label: "Θ34",
-    value: "Θ34"
-  },
-  {
-    label: "Θ35",
-    value: "Θ35"
-  },
-  {
-    label: "Θ36",
-    value: "Θ36"
-  },
-  {
-    label: "Θ37",
-    value: "Θ37"
-  },
-  {
-    label: "Θ38",
-    value: "Θ38"
-  },
-  {
-    label: "Θ39",
-    value: "Θ39"
-  },
-  {
     label: "Θ41",
     value: "Θ41"
+  },
+  {
+    label: "Θ42",
+    value: "Θ42"
+  },
+  {
+    label: "Θ43",
+    value: "Θ43"
   }
-  
 ];
 export default positions;
