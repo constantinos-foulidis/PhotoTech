75b72988a932ae813ef0ce3b1afd18d1279be234
constantinos <constantine_flds@hotmail.com>
2020-04-11 17:18:44 +0300

    fixing bad request

--- src/store/actions/Grafeio/photografers.js
+++ src/store/actions/Grafeio/photografers.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 import FormData from 'form-data';
 
-const baseUrl = 'http://localhost:4040/';
+const baseUrl = 'http://167.172.109.106/';
 const photografersUri = 'api/photo';
 const uniquePhotografer = '/under';
 const updateUri='/update';
