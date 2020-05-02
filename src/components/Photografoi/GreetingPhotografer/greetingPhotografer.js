bd5bae78eb2c2d0d7065dd83f7190cd26f937d8f
constantinos <constantine_flds@hotmail.com>
2020-05-02 20:18:21 +0300

    navigation drawer on Photografous component ready

--- src/components/Photografoi/GreetingPhotografer/greetingPhotografer.js
+++ src/components/Photografoi/GreetingPhotografer/greetingPhotografer.js
@@ -2,23 +2,24 @@ import React from 'react';
 //import { navigationToggleButton as NavigationToggleButton } from '../NavigationDrawer/NavigationDrawer'
 //import NavigationDrawer from "../../components/NavigationDrawer/NavigationDrawer";
 //import AppHeader from "../AppHeader/AppHeader";
-
+import { navigationToggleButton as NavigationToggleButton } from '../NavigationPhotografers/NavigationPhotografers'
+import NavigationPhotografers from "../NavigationPhotografers/NavigationPhotografers";
 const greetingPhotografer = (props) => {
  console.log(props);
    //<NavigationDrawer/>
    //<NavigationToggleButton/>
     return (
       <>
-
       <div className="row ml-5">
-
+          <NavigationPhotografers/>
         <div className="col-2">
-
+      <NavigationToggleButton/>
       </div>
       <div className="col">
       <div className="greetingMessage">Καλωσήρθες {JSON.parse(localStorage.getItem('userName'))}! </div>
       </div>
       </div>
+
       </>
   );
 }
--- src/components/Photografoi/photograferContainer/photograferContainer.js
+++ src/components/Photografoi/photograferContainer/photograferContainer.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { navigationToggleButton as NavigationToggleButton } from '../../Grafeio/NavigationGrafeio/NavigationGrafeio'
-import NavigationGrafeio from "../../Grafeio/NavigationGrafeio/NavigationGrafeio";
+
 import Row from 'react-bootstrap/Row';
 import PhotograferAppHeader from "../PhotograferAppHeader/photograferAppHeader";
 import { Switch,Route } from "react-router-dom";
@@ -8,14 +7,16 @@ import greetingPhotografer from "../GreetingPhotografer/greetingPhotografer";
 const PhotograferContainer = (props) => {
  console.log(props);
     return (
-      <>
+      <React.Fragment>
       <PhotograferAppHeader/>
+
+
       <Row>
         <Switch>
         <Route exact path="/photografer/greetings" component={greetingPhotografer} />
         </Switch>
       </Row>
-      </>
+      </React.Fragment>
   );
 }
 
