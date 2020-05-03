0a132565012d52dc1985bcde6df0057be2d7a0dd
constantinos <constantine_flds@hotmail.com>
2020-05-03 17:21:21 +0300

    create filter by date

--- src/components/Photografoi/Rantevou/rantevou.js
+++ src/components/Photografoi/Rantevou/rantevou.js
@@ -36,16 +36,18 @@ class Rantevou extends Component {
 
            }
   onChange = (date) => {
-    let temp=this.props.appointments;
+    if(this.props.appointmentsById !=null){
+      console.log(this.props.appointmentsById);
+    let temp=this.props.appointmentsById.Appointments;
 
-  let test=  temp.filter(appointments => new Date(parseInt(appointments.year,10),parseInt(appointments.month,10)-1,parseInt(appointments.day,10)).toLocaleDateString() === date.toLocaleDateString() );
+    let test=  temp.filter(appointments => new Date(parseInt(appointments.year,10),parseInt(appointments.month,10)-1,parseInt(appointments.day,10)).toLocaleDateString() === date.toLocaleDateString() );
     const year=date.toLocaleDateString().substring(4,9).replace('/','');
     const month=date.toLocaleDateString().substring(0,2).replace('/','');
     const day=date.toLocaleDateString().substring(2,4).replace('/','');
+    console.log(test[0]);
     this.setState({ date:date,filteredAppointmets:test,year:year,month:month,day:day});
   }
-
-
+}
 
   showDetailAppointments()  {
     console.log("mpika sto modal");
@@ -68,14 +70,30 @@ class Rantevou extends Component {
   }
 
   render() {
-
+   let appointment=null;
+   console.log(this.state.filteredAppointmets);
+  if(this.state.filteredAppointmets !=null){
+   appointment = (
+     this.state.filteredAppointmets.map((appointments,index) => (
+       <div  key={index} onClick={this.showDetailAppointments}>
+      <div className="row justify-content-center" >
+       <div className="col-1 mr-3">
+     <p className="smallText" >{appointments.time}</p>
+       </div>
+       <div className="col-1">
+     <p className="smallText" >{appointments.school}</p>
+       </div>
+       </div>
+     </div>
+   )))
+ }
 
     return (<React.Fragment>
 
       <div className="container mb-5">
       <h5 className="text-center">Ραντεβού</h5>
-        <div className="row">
-         <div className="col offset-3">
+        <div className="row ">
+         <div className="col-4 offset-3">
           <Calendar
            className="mb-5 ml-5 "
            onChange={this.onChange}
@@ -84,6 +102,12 @@ class Rantevou extends Component {
          />
          </div>
         </div>
+        <div className="row">
+          <div className="col">
+          <h5 className="text-center">{this.state.date.toLocaleDateString()}</h5>
+          </div>
+        </div>
+         {appointment}
       </div>
 
     </React.Fragment>)
@@ -92,7 +116,7 @@ class Rantevou extends Component {
 
 const mapStateToProps = (state, props) => {
   return {
-    sellers: state.sellers.filterSellers,
+    appointmentsById: state.photografers.appointmentsById,
     appointments: state.sellers.filterAppointments,
     loading: state.sellers.loading,
     SellerCode: state.loginGrafeiou.SellerCode,
